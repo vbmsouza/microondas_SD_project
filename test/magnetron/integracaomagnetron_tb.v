ecd767f3b179973f115e72de9c8b2d27792f2b04
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-30 00:14:28 -0300

    Update integracaomagnetron_tb.v

--- test/magnetron/integracaomagnetron_tb.v
+++ test/magnetron/integracaomagnetron_tb.v
@@ -1,16 +1,14 @@
 `timescale 1us/1ps
-module  Magnetron_tb();
+module  IntegracaoMagnetron_tb();
     reg comecan, paren, limpan, portafechada, tdone;
     wire m_on;
-    integer i;
+    integer y;
     Magnetron DUT(.comecan(comecan),.paren(paren),.limpan(limpan),.portafechada(portafechada),.tdone(tdone),.m_on(m_on));
-
     initial begin
-    
         $dumpfile("integracaomagnetron.v");
         $dumpvars();
-        for(i=0 ; i<3000; i++)begin
-            #10comecan = (i%2);
+        for(y=0 ; y<3000; y++)begin
+            #10comecan = (y%2);
             paren = $urandom%2;
             limpan = $urandom%2;
             portafechada =$urandom%2;
