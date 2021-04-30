c1c45f00089330ca88cf9760705596db7eb2b8cb
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-30 01:02:40 -0300

    Update integra_timer_and_control.v

--- HDL/entrada_timer_control/integra_timer_and_control.v
+++ HDL/entrada_timer_control/integra_timer_and_control.v
@@ -9,7 +9,7 @@ module controller (
 
 wire w1; 
 wire w2;
-    priority_encoder encoder(.d_out(out), .d_in(entrada), .enable(en), .Cin(Cin));
+    priority_encoder encoder(.d_out(out), .d_in(entrada), .en(en), .Cin(Cin));
     counter_delay delay(clock, Cin,w1);
 freq_converter converter(clock,w2);
 mux2x1 mux(en,w1,w2,pt);
