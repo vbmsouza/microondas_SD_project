c373d81fa72c72b9459e244504566a50c5131c40
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-29 21:53:13 -0300

    Create mod_6.v

---
+++ HDL/timer/mod_6.v
@@ -0,0 +1,21 @@
+module contador_0_5 (clk, enable, out, endout);
+    input clk, enable;
+    output reg [3:0] out;
+    output reg endout;
+
+    initial begin
+        out<=4'b0000;
+        endout<=1'b0;
+    end
+
+    always@(posedge clk && enable) begin
+        out <= out + 4'b0001;
+        endout <= 1'b0; 
+        if(out==4'b0101)begin
+            endout<=1'b1;
+            out<=4'b0000;
+        end
+
+    end
+    
+endmodule
