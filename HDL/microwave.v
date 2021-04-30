8e10c4307aa03947516ede2032a4124cf14ecf2a
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-30 11:02:12 -0300

    Update microwave.v

--- HDL/microwave.v
+++ HDL/microwave.v
@@ -1,31 +1,28 @@
 module micro(
-    input wire [9:0] keypad,
-    input wire clock,
-    input wire startn,
-    input wire stopn,
-    input wire clearn,
-    input wire door_closed,
-    output wire mag_on,
-    output wire [6:0] sec_ones_segs,
-    output wire [6:0] sec_tens_segs,
-    output wire [6:0] min_segs
+    input wire [9:0] teclado,
+    input wire clk,
+    input wire comecan,
+    input wire paren,
+    input wire limpan,
+    input wire portafechada,
+    output wire m_on,
+    output wire [6:0] sec_one_sec,
+    output wire [6:0] sec_ten_sec,
+    output wire [6:0] min_sec
 );
 
 wire magnetron;
-wire [3:0]d_to_data;
-wire loadn;
-wire pgt_to_timer_clock;
-wire [3:0] sec_ones;
-wire [3:0] sec_tens;
-wire [3:0] mins;
-wire timer_done;
+wire [3:0]d_data;
+wire Cin;
+wire pt_timer_clk;
+wire [3:0] sec_one;
+wire [3:0] sec_ten;
+wire [3:0] min;
+wire tdone;
+    controller controlador(teclado,magnetron, clk,d_data,Cin,pt_timer_clk);
+    timer time_device(d_data,Cin,limpan,pt_timer_clk,magnetron,sec_one,sec_ten,min,tdone);
+    priority_decoder4x7 decoder_device(sec_one,sec_ten,min,sec_one_sec,sec_ten_sec,min_sec);
+    Magnetron magnetron_device(comecan,paren,limpan,portafechada, tdone,magnetron);
 
-controller controlador(keypad,magnetron, clock,d_to_data,loadn,pgt_to_timer_clock);
-timer time_device(d_to_data,loadn,clearn,pgt_to_timer_clock,magnetron,sec_ones,sec_tens,mins,timer_done);
-decoder4x7 decoder_device(sec_ones,sec_tens,mins,sec_ones_segs,sec_tens_segs,min_segs);
-Magnetron magnetron_device(startn,stopn,clearn,door_closed, timer_done,magnetron);
-
-assign mag_on = magnetron;    
-
-
+assign m_on = magnetron;    
 endmodule
