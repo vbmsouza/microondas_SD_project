db6a00b7cdbbf87d547a07ed228aebac1260cac0
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-29 21:52:25 -0300

    Update Contadormod10.v

--- HDL/timer/Contadormod10.v
+++ HDL/timer/Contadormod10.v
@@ -1,40 +1,21 @@
-module timer_ten(input wire [3:0] data, input wire loadn,input wire clk,input wire clrn, input wire en,
-               output reg [3:0]  out, output wire tc, output reg zero );
+module contador_0_9 (clk, enable, out, endout);
+    input clk, enable;
+    output reg [3:0] out;
+    output reg endout;
 
-assign tc = (en & ~out[0] & ~out[1] & ~out[2] & ~out[3]);
-initial begin
-    out = 0;
-end
-
-always @(posedge clk,negedge clrn)
-    begin
-        if(~clrn)
-        begin
-            out <= 0;
-            zero <= 1;
-        end
-        else if(~loadn)
-            begin
-                out <= data;
-                zero <= (data==0)? 1 : 0;
-            end
-        else if(en)
-        begin
-            if(out==0)
-            begin
-                out <= 9;
-                zero <= 0;
-            end
-            else if(out==1)
-            begin
-                out <= out-1;
-                zero <= 1;
-            end
-            else
-            begin
-                out <= out-1;
-                zero <= 0;
-            end
-        end
+    initial begin
+        out<=4'b0000;
+        endout<=1'b0;
     end
+
+    always@(posedge clk && enable) begin
+        out <= out + 4'b0001;
+        endout <= 1'b0; 
+        if(out==4'b1001)begin
+            endout<=1'b1;
+            out<=4'b0000;
+        end
+
+    end
+    
 endmodule
