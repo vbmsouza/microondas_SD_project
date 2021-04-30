3671e19b7bb0e48e2a4ae4d8bcc2cbe3c46c3ad6
josevinicius1209 <61716284+josevinicius1209@users.noreply.github.com>
2021-04-29 23:39:47 -0300

    Update and rename timer_tb.v to integracao_testb.v

--- test/timer/integracao_testb.v
+++ test/timer/integracao_testb.v
@@ -1,19 +1,19 @@
 `timescale 1ms/1ps
 
-module timer_tb();
+module integracao_tb();
 
 reg [3:0] in = 4'b0111;
 wire [3:0] out;
-reg loadn;
-reg clrn=1'b1; 
+reg Cin;
+reg Cn=1'b1; 
 reg clock=1'b0;
-reg en = 1'b1;
-wire [3:0] sec_ones;
-wire [3:0] sec_tens;
-wire [3:0] mins;
+reg z = 1'b1;
+    wire [3:0] sec_unidade;
+    wire [3:0] sec_decimal;
+wire [3:0] min;
 wire zero;
 
-timer DUT(in,loadn,clrn,clock, en,sec_ones,sec_tens,mins, zero);
+    timer DUT(in,Cin,Cn,clock, z,sec_unidade,sec_decimal,min, zero);
 
 always #5 clock = ~clock;
 
@@ -22,17 +22,17 @@ initial begin
     $dumpvars;
     //Carregar 1:99 no contador zera a contagem quando chega em 50 segundos (usando o clear) e encerra o processo
     //Testa a funcionalidade do do contador, do load, do clear e de carregar um número entre 60-99
-    loadn = 0;
+    Cin = 0;
     in = 1;
     #10 in = 9;
     #10 in = 9;
-    #10 loadn=1;
+    #10 Cin=1;
     
 end
 
-always @ (zero,sec_ones,sec_tens,mins) begin
-    if(sec_ones==0 && sec_tens ==5 && mins == 0) clrn <= 0;
-    if(loadn ==1 && zero==1) $finish;
+    always @ (zero,sec_unidade,sec_decimal,min) begin
+        if(sec_unidade==0 && sec_tens ==5 && min == 0) Cn <= 0;
+    if(Cin ==1 && zero==1) $finish;
 end
 
 endmodule
