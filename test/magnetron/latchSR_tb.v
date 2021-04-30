e91c94daceaec79e3160b251de063dc24ca1db06
josevinicius1209 <61716284+josevinicius1209@users.noreply.github.com>
2021-04-29 23:48:23 -0300

    Update and rename latchsr_tb.v to latchSR_tb.v

---
+++ test/magnetron/latchSR_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns/1ps
+module latchsr_tb();
+    integer i;
+	reg Reset, Set;
+	wire mon;
+
+	latchsr DUT(.Reset(Reset), .Set(Set), .m_on(mon));
+	
+    initial
+		begin
+		
+			$dumpfile("latchSR_tb.vcd");
+			$dumpvars();
+            for(i = 0; i<30000; i++)begin
+               #10 Set = i%2;
+                Reset = 1-Set; 
+            end
+		end
+
+endmodule
