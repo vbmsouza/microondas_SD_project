393373212a747bb986b8c2fee2d511ed962244b9
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-30 00:09:06 -0300

    Update integracaomagnetron.v

--- HDL/magnetron/integracaomagnetron.v
+++ HDL/magnetron/integracaomagnetron.v
@@ -1,7 +1,4 @@
-module  IntegracaoMagnetron(
-    input wire comecan, paren, limpan, portafechada, tdone,
-    output wire m_on
-);
+module  IntegracaoMagnetron( input wire comecan, paren, limpan, portafechada, tdone, output wire m_on);
     wire Reset, Set;
     logicC DUT1(.comecan(comecan), .paren(paren), .limpan(limpan), .portafechada(portafechada), .tdone(tdone), .Set(Set), .Reset(Reset));
     latchsr DUT2(Set, Reset, m_on);  
