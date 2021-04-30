bf0e030b7e509d261a9472d647855d18d5bbb4b9
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-30 00:29:39 -0300

    Update decoder.v

--- HDL/decoder_driver/decoder.v
+++ HDL/decoder_driver/decoder.v
@@ -1,29 +1,20 @@
-module decoder4x7(
-  input wire [3:0] sec_ones, sec_tens, min,
-  output wire [6:0] sec_ones_segs, sec_tens_segs, min_segs);
-  
-  wire en_secs_ones;
-  wire en_secs_tens;
+module decoder( input wire [3:0] sec_on, sec_t, min, output wire [6:0] sec_on_secs, sec_t_secs, min_secs);
+  wire en_secs_on;
+  wire en_secs_t;
   wire ground;
   
-  
-  decoderUnit Dec1(.binary_in(sec_ones), .decoder_out(sec_ones_segs), .en_in(en_secs_ones) , .en_out(ground));
-  decoderUnit Dec2(.binary_in(sec_tens), .decoder_out(sec_tens_segs), .en_in(en_secs_tens) , .en_out(en_secs_ones));
-  decoderUnit Dec3(.binary_in(min), .decoder_out(min_segs), .en_in(1'b0), .en_out(en_secs_tens));
+  decoderUnit Dec1(.binary_in(sec_on), .decoder_out(sec_on_secs), .en_in(en_secs_on) , .en_out(ground));
+  decoderUnit Dec2(.binary_in(sec_t), .decoder_out(sec_t_secs), .en_in(en_secs_t) , .en_out(en_secs_on));
+  decoderUnit Dec3(.binary_in(min), .decoder_out(min_secs), .en_in(1'b0), .en_out(en_secs_t));
   
 endmodule
 
-
 module decoderUnit(
-  input wire [3:0] binary_in   , //  4 bit binary input
-  output wire [6:0] decoder_out , //  7-bit out 
+  input wire [3:0] binary_in   ,
+  output wire [6:0] decoder_out ,
   input wire en_in,
   output wire en_out
 ); 
- 
- //Decoder que mapeia para um binário para um bus de bits do display de 7 segmentos na forma abcdefg
- //onde ALTO(1) é o sinal de led ligado e o BAIXO(0), desligado.
-  
   assign decoder_out = (binary_in==0 && en_in) ? 10'b1111110:
                        (binary_in==0 && ~(en_in)) ? 10'b0000000:
                        (binary_in==1) ? 10'b0110000:
