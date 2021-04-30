d81f24f437259cfd33b65594c9438ad864f21bb0
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-29 22:40:34 -0300

    Create freq100.v

---
+++ HDL/entrada_timer_control/freq100.v
@@ -0,0 +1,24 @@
+module freq_converter(input wire clk, output reg new_clk); //divide by 100
+
+reg [7:0] cont;
+
+initial begin
+    cont = 0;
+    new_clk = 0; 
+end
+
+always @(posedge clk)
+begin
+    if(cont==49)
+    begin
+        cont = 0;
+        new_clk = ~new_clk;
+    end
+    else
+    begin
+        cont = cont+1;
+    end
+
+end
+
+endmodule
