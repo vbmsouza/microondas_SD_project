e36c649fa9c95cc589cd35e9ae07e11ab46701ea
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-29 23:24:43 -0300

    Update Contador_mod_6.v

--- HDL/timer/Contador_mod_6.v
+++ HDL/timer/Contador_mod_6.v
@@ -1,25 +1,25 @@
-module timer_six(input wire [3:0] data, input wire loadn,input wire clk,input wire clrn, input wire en,
-               output reg [3:0]  out, output wire tc, output reg zero );
+module contador_mod6(input wire [3:0] in, input wire Cin, input wire clk,input wire Cn, input wire z,
+                     output reg [3:0]  out, output wire x, output reg zero );
 
-assign tc = (en & ~out[0] & ~out[1] & ~out[2] & ~out[3]);
+  assign x = (z & ~out[0] & ~out[1] & ~out[2] & ~out[3]);
 
 initial begin
     out  = 0;
 end
 
-always @(posedge clk,negedge clrn)
+  always @(posedge clk,negedge Cn)
     begin
-        if(~clrn)
+      if(~Cn)
         begin
             out <= 0;
             zero <= 1;
         end
-        else if(~loadn)
+      else if(~Cin)
             begin
-                out <= data;
-                zero <= (data==0)? 1 : 0;
+                out <= in;
+              zero <= (in==0)? 1 : 0;
             end
-        else if(en)
+      else if(z)
         begin
             if(out==0)
             begin
