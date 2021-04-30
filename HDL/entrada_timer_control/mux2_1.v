f6c8e024dfcf5880e65c801cdf0cd210eef918d4
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-29 22:38:42 -0300

    Update mux2_1.v

--- HDL/entrada_timer_control/mux2_1.v
+++ HDL/entrada_timer_control/mux2_1.v
@@ -1,17 +1,8 @@
-module multiplexer (a, b, c, d, s, o); 
-  input a,b,c,d; 
-  input [1:0] s; 
-  output o; 
-  reg o; 
- 
-  always @(a or b or c or d or s) 
-  begin 
-    case (s) 
-      2'b00   : o = a; 
-      2'b01   : o = b; 
-      2'b10   : o = c; 
-      default : o = d; 
-    endcase 
-  end
-  
+module mux2x1 (input wire Sel,
+input wire  a,
+input wire  b,
+output wire out);
+
+assign out = (Sel ==0 ?a:b); 
+
 endmodule
