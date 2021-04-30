bffcf35a1961ff6fcf23a3177bef68eca5329ba7
vbmsouza <32920217+vbmsouza@users.noreply.github.com>
2021-04-30 10:23:47 -0300

    Update cont_delay.v

--- HDL/entrada_timer_control/cont_delay.v
+++ HDL/entrada_timer_control/cont_delay.v
@@ -1,24 +1,19 @@
-module counter_delay ( input wire clk, input wire clr, output reg out);
+module counterdlay ( input wire clk, input wire clr, output reg out);
 
-reg [3:0] counter=0;
-reg pushed;
+reg [2:0] counter=0;
 
-    initial pushed = 0;
 
-always @(posedge clr) begin
 
-    counter = 0;
-    pushed <= 1;
-end
-
-always@(clk)
-begin
-    if(pushed)
-    begin
-    if(counter<14) counter = counter+1;
-    else pushed = 0;
-    out = ((counter==8 |counter==9) & pushed) ? 1:0;
+always @(posedge clk) begin
+    if(clr) begin
+        out <= 0;
+        contr<=0;
     end
-end
+    else
+    begin
+        if(contr<7) countr <= contr+1;
+        out <= (contr==4) ? 1:0;
+    end
     
+end
 endmodule
